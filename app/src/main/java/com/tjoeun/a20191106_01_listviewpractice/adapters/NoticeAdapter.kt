bbc2bb9a5879b101153af45b685d57e204b0eb70
sjsid79 <wjdtlr0723>
2019-11-06 20:50:13 +0900

    주석 추가

--- app/src/main/java/com/tjoeun/a20191106_01_listviewpractice/adapters/NoticeAdapter.kt
+++ app/src/main/java/com/tjoeun/a20191106_01_listviewpractice/adapters/NoticeAdapter.kt
@@ -30,11 +30,15 @@ class NoticeAdapter(context:Context, resource:Int, list:ArrayList<NoticeData>)
         // row에서 tempRow는 절대 null 아니라고 하면서 대입
         var row = tempRow!!     // !! -> tempRow는 절대 null이 아니라고 우기는거야.
 
+        // 근거 데이터 변수 추출
         var data = mList.get(position)
+        
+        // 각 줄에서 데이터를 출력할 View 찾아오기
         var titleTxt = row.findViewById<TextView>(R.id.noticeTxt)
         var dateTxt = row.findViewById<TextView>(R.id.dateTxt)
         var contextTxt = row.findViewById<TextView>(R.id.contentTxt)
 
+        // View와 Data를 연결
         titleTxt.text = data.title
         dateTxt.text = data.date
         contextTxt.text = data.content
