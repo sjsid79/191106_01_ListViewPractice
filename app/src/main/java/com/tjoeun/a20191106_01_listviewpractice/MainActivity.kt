4b50b4ca1de924967f5ab580d9bf2e07e7a6f40f
sjsid79 <wjdtlr0723>
2019-11-06 20:38:07 +0900

    어댑터 클래스 구조 생성

--- app/src/main/java/com/tjoeun/a20191106_01_listviewpractice/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191106_01_listviewpractice/MainActivity.kt
@@ -2,11 +2,33 @@ package com.tjoeun.a20191106_01_listviewpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tjoeun.a20191106_01_listviewpractice.datas.NoticeData
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
+
+    var noticeList = ArrayList<NoticeData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        addNotices()
+    }
+
+    fun addNotices(){
+        noticeList.add(NoticeData("1번 공지","1번 내용입니다.","2019-01-06"))
+        noticeList.add(NoticeData("2번 공지","2번 내용입니다.","2019-02-06"))
+        noticeList.add(NoticeData("3번 공지","3번 내용입니다.","2019-03-06"))
+        noticeList.add(NoticeData("4번 공지","4번 내용입니다.","2019-04-06"))
+        noticeList.add(NoticeData("5번 공지","5번 내용입니다.","2019-05-06"))
+        noticeList.add(NoticeData("6번 공지","6번 내용입니다.","2019-06-06"))
+        noticeList.add(NoticeData("7번 공지","7번 내용입니다.","2019-07-06"))
+        noticeList.add(NoticeData("8번 공지","8번 내용입니다.","2019-08-06"))
+        noticeList.add(NoticeData("9번 공지","9번 내용입니다.","2019-09-06"))
+        noticeList.add(NoticeData("10번 공지","10번 내용입니다.","2019-10-06"))
+        noticeList.add(NoticeData("11번 공지","11번 내용입니다.","2019-11-06"))
+        noticeList.add(NoticeData("12번 공지","12번 내용입니다.","2019-12-06"))
+        noticeList.add(NoticeData("13번 공지","13번 내용입니다.","2019-12-25"))
     }
 }
---
+++ app/src/main/java/com/tjoeun/a20191106_01_listviewpractice/adapters/NoticeAdapter.kt
@@ -0,0 +1,35 @@
+package com.tjoeun.a20191106_01_listviewpractice.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.tjoeun.a20191106_01_listviewpractice.R
+import com.tjoeun.a20191106_01_listviewpractice.datas.NoticeData
+
+class NoticeAdapter(context:Context, resource:Int, list:ArrayList<NoticeData>)
+    : ArrayAdapter<NoticeData> (context, R.layout.notice_list_item, list){
+
+    var mContext = context
+    var mList = list
+    var inf = LayoutInflater.from(mContext)
+
+    constructor(context: Context, list: ArrayList<NoticeData>)
+            : this(context, R.layout.notice_list_item, list)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+
+        if (tempRow == null) {
+            tempRow = inf.inflate(R.layout.notice_list_item, null)
+        }
+
+        // tempRow가 null인 경우를 위에서 대비했으니,
+        // row에서 tempRow는 절대 null 아니라고 하면서 대입
+        var row = tempRow!!     // !! -> tempRow는 절대 null이 아니라고 우기는거야.
+        return  row
+    }
+
+
+}
