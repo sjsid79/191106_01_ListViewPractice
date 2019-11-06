8d0bf1711c6f07aa11b7b83fa445c9e44c087a85
sjsid79 <wjdtlr0723>
2019-11-06 19:56:20 +0900

    데이터 클래스 생성

---
+++ app/src/main/java/com/tjoeun/a20191106_01_listviewpractice/datas/NoticeData.kt
@@ -0,0 +1,13 @@
+package com.tjoeun.a20191106_01_listviewpractice.datas
+
+class NoticeData(inputTitle:String, inputContent:String, writeDate:String) {
+
+    var title = inputTitle
+    var content = inputContent
+    var date = writeDate
+
+//    init {
+//
+//    }
+
+}
